d5dbc0366a4c4ea54c5246f0092692edae475dc8
Daniel <uzo1209@gmail.com>
2023-11-28 16:48:23 +0100

    generic commit

--- src/app/service/posts.service.ts
+++ src/app/service/posts.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@angular/core';
+import { Post } from '../models/post';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostsService {
-
+  posts: Post[] = [];
+  
   constructor() { }
 }
