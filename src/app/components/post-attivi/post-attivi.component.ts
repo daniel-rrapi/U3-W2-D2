fabdcb7472f142ba2756c9e83ce804fe60f44a0c
Daniel <uzo1209@gmail.com>
2023-11-28 16:34:59 +0100

    second commit

---
+++ src/app/components/post-attivi/post-attivi.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-post-attivi',
+  templateUrl: './post-attivi.component.html',
+  styleUrls: ['./post-attivi.component.scss']
+})
+export class PostAttiviComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/components/post-disattivati/post-disattivati.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-post-disattivati',
+  templateUrl: './post-disattivati.component.html',
+  styleUrls: ['./post-disattivati.component.scss']
+})
+export class PostDisattivatiComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
